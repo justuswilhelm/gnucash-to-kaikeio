94122d33ca96f2c1839747daa467c4754e587031
Justus Perlwitz <mail@justusperlwitz.com>
2023-01-27 14:33:35 +0900

    Pick apart journal entry building

--- gntoka/journal.py
+++ gntoka/journal.py
@@ -1,4 +1,7 @@
 """Build journal entries."""
+from datetime import (
+    date,
+)
 from decimal import (
     Decimal,
 )
@@ -10,19 +13,104 @@ from .types import (
     JournalEntries,
     JournalEntry,
     JournalEntryCounter,
+    Split,
     TransactionSplit,
 )
 
 
-def build_journal_entries(
-    counter: JournalEntryCounter, tx: TransactionSplit
+def make_journal_entry(
+    伝票番号: str,
+    伝票日付: date,
+    借方科目コード: str,
+    借方科目名称: str,
+    借方補助コード: str,
+    借方補助科目名称: str,
+    借方金額: Decimal,
+    借方消費税額: Decimal,
+    貸方科目コード: str,
+    貸方科目名称: str,
+    貸方補助コード: str,
+    貸方補助科目名称: str,
+    貸方金額: Decimal,
+    貸方消費税額: Decimal,
+    メモ: str,
+) -> JournalEntry:
+    """Make a JournalEntry."""
+    return JournalEntry(
+        伝票番号=伝票番号,
+        行番号="1",
+        伝票日付=util.format_date(伝票日付),
+        借方科目コード=借方科目コード,
+        借方科目名称=借方科目名称,
+        借方補助コード=借方補助コード,
+        借方補助科目名称=借方補助科目名称,
+        借方部門コード="0",
+        借方部門名称="",
+        借方課税区分="0",
+        借方事業分類="0",
+        借方消費税処理方法="3",
+        借方消費税率="0%",
+        借方金額=借方金額,
+        借方消費税額=Decimal("0"),
+        貸方科目コード=貸方科目コード,
+        貸方科目名称=貸方科目名称,
+        貸方補助コード=貸方補助コード,
+        貸方補助科目名称=貸方補助科目名称,
+        貸方部門コード="0",
+        貸方部門名称="",
+        貸方課税区分="",
+        貸方事業分類="0",
+        貸方消費税処理方法="3",
+        貸方消費税率="0%",
+        貸方金額=貸方金額,
+        貸方消費税額=貸方消費税額,
+        摘要="",
+        補助摘要="",
+        メモ=メモ,
+        付箋１="0",
+        付箋２="0",
+        伝票種別="0",
+    )
+
+
+def build_simple_journal_entry(
+    number: str, debit: Split, credit: Split
+) -> JournalEntry:
+    """Build a simple journal entry."""
+    date = credit.transaction.date
+    memo = " ".join(
+        [
+            credit.transaction.description,
+            credit.memo,
+            debit.memo,
+        ]
+    ).replace("\xa0", " ")
+    return make_journal_entry(
+        伝票番号=number,
+        伝票日付=date,
+        借方科目コード=debit.account.account,
+        借方科目名称=debit.account.account,
+        借方補助コード=debit.account.account_supplementary,
+        借方補助科目名称=debit.account.account_supplementary_name,
+        借方金額=debit.value,
+        借方消費税額=Decimal("0"),
+        貸方科目コード=credit.account.account,
+        貸方科目名称=credit.account.account_name,
+        貸方補助コード=credit.account.account_supplementary,
+        貸方補助科目名称=credit.account.account_supplementary_name,
+        貸方金額=credit.value,
+        貸方消費税額=Decimal("0"),
+        メモ=memo,
+    )
+
+
+def build_composite_journal_entry(
+    counter: JournalEntryCounter,
+    debits: TransactionSplit,
+    credits: TransactionSplit,
 ) -> JournalEntries:
-    """Build journal entries given a transaction."""
+    """Build a composite journal entry using the 複合 intermediary."""
     result = []
-    assert len(tx) > 1
-    assert sum(split.value for split in tx) == Decimal(0), tx
-    debits = list(util.get_debits(tx))
-    credits = list(util.get_credits(tx))
     # Scenario one, one credit split that funds n debit splits
     if len(debits) > len(credits):
         assert len(credits) == 1
@@ -32,7 +120,7 @@ def build_journal_entries(
         swap = True
     # Scenario two, one debit split that funds n credit splits
     else:
-        assert len(debits) == 1, tx
+        assert len(debits) == 1, f"{debits} {credits}"
         larger_side = credits
         smaller_side = debits[0]
         # Magic variable, do not touch
@@ -100,3 +188,21 @@ def build_journal_entries(
         )
         result.append(d)
     return result
+
+
+def build_journal_entries(
+    counter: JournalEntryCounter, tx: TransactionSplit
+) -> JournalEntries:
+    """Build journal entries given a transaction."""
+    assert len(tx) > 1
+    assert sum(split.value for split in tx) == Decimal(0), tx
+    debits = list(util.get_debits(tx))
+    credits = list(util.get_credits(tx))
+    if len(debits) == 1 and len(credits) == 1:
+        # Simple split
+        (debit,) = debits
+        (credit,) = credits
+        return [build_simple_journal_entry(str(next(counter)), debit, credit)]
+        # Compound split
+    else:
+        return build_composite_journal_entry(counter, debits, credits)
