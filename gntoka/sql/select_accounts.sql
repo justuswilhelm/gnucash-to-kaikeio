a530fd3240115bdec7987edf2633f2f8e0e5507e
Justus Perlwitz <mail@justusperlwitz.com>
2023-02-02 10:39:28 +0900

    Fix placeholder selection

--- gntoka/sql/select_accounts.sql
+++ gntoka/sql/select_accounts.sql
@@ -1,19 +1,27 @@
 select accounts.guid
-, case parent_accounts.code
-    when '' then accounts.code
+, case parent_accounts.placeholder
+    when 1 then accounts.code
     else parent_accounts.code
     end as code
-, case parent_accounts.code
-    when '' then accounts.name
+, case parent_accounts.placeholder
+    when 1 then accounts.name
     else parent_accounts.name
     end as name
-, case parent_accounts.code
-    when '' then null
+, case parent_accounts.placeholder
+    when 1 then null
     else accounts.code
     end as supplementary_code
-, case parent_accounts.code
-    when '' then null
+, case parent_accounts.placeholder
+    when 1 then null
     else accounts.name
     end as supplementary_name
 from accounts
-inner join accounts as parent_accounts on accounts.parent_guid = parent_accounts.guid
+inner join (
+    select guid
+    , code
+    , name
+    , placeholder
+    from accounts
+)
+as parent_accounts on accounts.parent_guid = parent_accounts.guid
+where accounts.placeholder = 0
